85456a61ca90d757263f2515c09a63d04e11a5b0
Cristian Ariza <cristianurba@gmail.com>
2020-03-14 18:34:44 +0100

    Tabla profesores: getAll, create y deleteById

---
+++ models/profesor.js
@@ -0,0 +1,32 @@
+const getAll = () => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from profesores', (err, rows) => {
+            if (err) reject(err);
+            resolve(rows);
+        })
+    })
+}
+
+const create = ({ nombre, experiencia }) => {
+    return new Promise((resolve, reject) => {
+        db.query('insert into profesores (nombre, experiencia) values (?,?)', [nombre, experiencia], (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+};
+
+const deleteById = (pProfesorId) => {
+    return new Promise((resolve, reject) => {
+        db.query('delete from profesores where id = ?', [pProfesorId], (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        })
+    })
+}
+
+module.exports = {
+    getAll: getAll,
+    create: create,
+    deleteById: deleteById
+}
--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiClientesRouter = require('./api/clientes');
 const apiEjerciciosRouter = require('./api/ejercicios');
+const apiProfesoresRouter = require('./api/profesores')
 
 router.use('/clientes', apiClientesRouter);
 router.use('/ejercicios', apiEjerciciosRouter);
+router.use('/profesores', apiProfesoresRouter);
 
 module.exports = router;
