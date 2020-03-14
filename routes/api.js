8aa98bcad12c169b6908987481bda3182e47dc01
Cristian Ariza <cristianurba@gmail.com>
2020-03-14 17:54:40 +0100

    Tabla Ejercicios: getAll, create y deleteById

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const apiClientesRouter = require('./api/clientes');
+const apiEjerciciosRouter = require('./api/ejercicios');
 
 router.use('/clientes', apiClientesRouter);
+router.use('/ejercicios', apiEjerciciosRouter);
 
 module.exports = router;
